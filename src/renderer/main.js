3c0e775c2ded862e3f4b4e260f5ec061b7b762c1
maxoyed <maxoyed@gmail.com>
2020-08-06 22:01:57 +0800

    Enhance: 删除无用依赖

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,12 +1,12 @@
 import Vue from "vue";
-import axios from "axios";
+// import axios from "axios";
 import Buefy from "buefy";
 import "buefy/dist/buefy.css";
 
 import App from "./App";
 
 if (!process.env.IS_WEB) Vue.use(require("vue-electron"));
-Vue.http = Vue.prototype.$http = axios;
+// Vue.http = Vue.prototype.$http = axios;
 Vue.config.productionTip = false;
 Vue.use(Buefy);
 
